94240ca3892e61283021c8d43141f965600d2b16
Unknown <xin.gao.au@outlook.com>
2018-12-14 18:06:49 +1100

    update env setting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { GlobalStyle } from './style';
 import Header from './common/header';
 import Home from './pages/home';
 import Detail from './pages/detail/loadable.js';
@@ -14,12 +15,13 @@ class App extends Component {
     	<Provider store={store}>
       	<BrowserRouter>
       		<div>
-            <Header />
-      			<Route path='/' exact component={Home}></Route>
-            <Route path='/login' exact component={Login}></Route>
-            <Route path='/write' exact component={Write}></Route>
-      			<Route path='/detail/:id' exact component={Detail}></Route>
-      		</div>
+                <GlobalStyle />
+                <Header />
+                    <Route path='/' exact component={Home}></Route>
+                    <Route path='/login' exact component={Login}></Route>
+                    <Route path='/write' exact component={Write}></Route>
+                    <Route path='/detail/:id' exact component={Detail}></Route>
+                </div>
       	</BrowserRouter>
       </Provider>
     );
